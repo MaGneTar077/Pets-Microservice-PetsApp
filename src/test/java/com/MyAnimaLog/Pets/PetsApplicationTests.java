b3fec3a98736aaab0d3565c319c5db21ec725d3d
Luis Santiago Gonzalezrubio <lgsantiago07@gmail.com>
2026-04-01 15:56:39 -0500

    Configuracion inicial del proyecto

---
+++ src/test/java/com/MyAnimaLog/Pets/PetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyAnimaLog.Pets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
